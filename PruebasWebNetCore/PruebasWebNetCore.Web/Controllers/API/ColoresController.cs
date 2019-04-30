0a24317feff19f43a1923707671d64ea90edf872
Oscar Claros <a2016114312@estudiantes.upsa.edu.bo>
2019-04-30 17:09:17 -0400

    add Api controller

---
+++ PruebasWebNetCore/PruebasWebNetCore.Web/Controllers/API/ColoresController.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace PruebasWebNetCore.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using PruebasWebNetCore.Web.Data.Repositories;
+
+    [Route("api/[Controller]")]
+    public class ColoresController : Controller
+    {
+        private readonly IColorRepository colorRepository;
+
+        public ColoresController(IColorRepository colorRepository)
+        {
+            this.colorRepository = colorRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetColores()
+        {
+            return Ok(this.colorRepository.GetAll());
+        }
+
+
+    }
+}
